9f2d6a1807285c230af73398a20a41679dac1809
Justin King <justin.king6@msubillings.edu>
2025-11-24 12:00:32 -0700

    Add initial project structure with main application and utility classes
    - Created .settings/org.eclipse.jdt.core.prefs to configure Java compiler settings.
    - Added .vscode/settings.json for project source paths and output configuration.
    - Introduced README.md with project setup and folder structure details.
    - Implemented App class as the entry point of the application.
    - Added Constants class for ship specifications.
    - Developed BetterWindow class for window management with preferences.
    - Created FadingLabel class for text fading effects.
    - Included binary files for compiled classes and JUnit library.

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // and so it begins...
+    }
+}
---
+++ src/Util/Constants.java
@@ -0,0 +1,9 @@
+package Util;
+
+import java.util.Map;
+
+public class Constants {
+    public final static Map<String, Integer> shipSpecifications = Map.of("Carrier", 5, "Battleship", 4, "Destroyer", 3, "Submarine", 3, "Patrol Boat", 2);
+    
+	private Constants(){}
+}
